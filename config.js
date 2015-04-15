35c6eac424d2cb9d74315c77653f753c1c44c771
Mary Dickson <mdickson@gmail.com>
2015-04-15 15:30:53 -0700

    first commit

---
+++ config.js
@@ -0,0 +1,7 @@
+// Put your own Twitter App keys here. See README.md for more detail.
+module.exports = {
+  consumer_key:         'n2pT9MWUifGXnmEV6giQ8CJTJ',
+  consumer_secret:      'WhDF7ynjd7zpguszuqk5mti8aFW1cwuWBppu2f67wEmRJ5R9Pd',
+  access_token:         '3011975700-Jv8OndpIZKII80KYtEl8kel3MH0BHvYY3D8xsPh',
+  access_token_secret:  'usNgys17H3OPtiUWDf6mR2vPV52UHJBRc1YVLQsYRNZz7'
+}
