d0e0819f463a5bb2d1fa6a9cc0afd4066d11c313
Mary Dickson <mdickson@gmail.com>
2015-04-20 15:55:15 -0700

    update gitignore folder and bot search terms

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var Twit = require('twit');
 var T = new Twit(require('./config.js'));
 
 // This is the URL of a search for the latest tweets on the '#achievementunlocked' hashtag.
-var hashTagSearch = {q: "#achievementunlocked -@gridminigames", count: 10, result_type: "recent"};
+var hashTagSearch = {q: "#achievementunlocked -gridminigames", count: 10, result_type: "recent"};
 
 // This function finds the latest tweet with the #mediaarts hashtag, and retweets it.
 function retweetLatest() {
